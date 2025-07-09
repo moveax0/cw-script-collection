5cc45ccede04ab826c1dc2944ce813c7fcf06e00
moveax0 <owser0133@gmail.com>
2025-07-09 02:06:11 +0200

    Update infinite_drop.asm

--- asm/infinite_drop.asm
+++ asm/infinite_drop.asm
@@ -1,31 +1,20 @@
 [ENABLE]
-//code from here to '[DISABLE]' will be used to enable the cheat
 alloc(newmem,2048)
 label(returnhere)
-label(originalcode)
-label(exit)
 
-newmem: //this is allocated memory, you have read,write,execute access
-//place your code here
-
-originalcode:
-nop // dec [ecx]
-cmp dword ptr [ecx],00
-
-exit:
-jmp returnhere
+newmem:
+  cmp dword ptr [ecx],00
+  jmp returnhere
 
 "Cube.exe"+2F140:
-jmp newmem
+  jmp newmem
 returnhere:
 
-
- 
- 
 [DISABLE]
-//code from here till the end of the code will be used to disable the cheat
+
 dealloc(newmem)
+
 "Cube.exe"+2F140:
-db FF 09 83 39 00
+  db FF 09 83 39 00
 //dec [ecx]
 //cmp dword ptr [ecx],00
